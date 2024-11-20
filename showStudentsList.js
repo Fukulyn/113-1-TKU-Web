2f2151b90335a88d78af2a985fcde67edb09ac84
Fukulyn <a098838628@gmail.com>
2024-11-20 22:07:26 +0800

    Update

--- showStudentsList.js
+++ showStudentsList.js
@@ -10,6 +10,7 @@
     (async () => {
         const client = new MongoClient(uri);
     
+
     try {
         // 連接到 MongoDB
         await client.connect();
@@ -19,18 +20,11 @@
         const collection = db.collection(collectionName);
 
         // 讀取 CSV 檔案
-        const results = [];
-        fs.createReadStream('studentslist.csv') // CSV 檔案路徑
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', async () => {
-                // 插入資料到 MongoDB
-                const insertResult = await collection.insertMany(results);
-                console.log('成功插入 ${insertResult.insertedCount} 筆資料！');
-
-                // 關閉連接
+        const results = await collection.find().toArray();
+        for (each of results){
+            console.log(each.姓名);
+        }
                 await client.close();
-            });
     } catch (error) {
         console.error("發生錯誤：", error);
     }
